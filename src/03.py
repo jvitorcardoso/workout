0d9508f2dc0abc2f572cd26fcc12b8ec7ca951c5
jvitorcardoso <aspirantevitor@gmail.com>
2024-12-15 19:05:25 -0300

    improvements to exerc 03

--- src/03.py
+++ src/03.py
@@ -3,16 +3,19 @@
 """My solution to exercise 3, chapter 1: Run timing"""
 
 def run_timing() -> str:
+    """Asks the user repeatedly for numeric input.
+    Prints the average time and number of runs.
+    """
     total, count = 0, 0
 
     while kilometers := input("Enter 10 km run time: "):
-        if kilometers != '':
+        if kilometers:
             total += float(kilometers)
             count += 1
         else:
             break
             
-    return f'Average of {round((total / count), 2)}, over {count} runs'
+    return f'Average of {(total / count):.2f}, over {count} runs'
 
 
 print(run_timing())
